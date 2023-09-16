14efba4149ae9cb17aab671fa14f708e69d71f62
S <c8949053@lowes.com>
2023-09-16 22:24:55 +0530

    new changes

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,23 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    public Boat(){
+
+    }
+
+    public Boat(String name,int capacity){
+        this.capacity = capacity;
+        this.name = name;
+    }
+
+    public String getVehicleName() {
+        return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
